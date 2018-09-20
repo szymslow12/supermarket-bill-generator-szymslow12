eaec113c1aa8b79c845a212103f02a8d4e2c560a
szymslow12 <szym.slow12@gmail.com>
2018-09-20 09:51:47 +0200

    add App.java with main()

---
+++ src/main/java/com/codecool/supermarket/App.java
@@ -0,0 +1,9 @@
+package com.codecool.supermarket;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}

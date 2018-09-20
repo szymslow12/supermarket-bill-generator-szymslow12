b0a4386d853a72a2ef45d3ddca5c97526602f758
szymslow12 <szym.slow12@gmail.com>
2018-09-20 10:19:13 +0200

    add shouldRemoveProduct, shouldNotRemoveNotExistProduct

--- src/test/com/codecool/supermarket/model/BasketTest.java
+++ src/test/com/codecool/supermarket/model/BasketTest.java
@@ -11,7 +11,7 @@ class BasketTest {
         basket.add(new Product("product", 1));
         basket.add(new Product("product2", 2));
         basket.add(new Product("product3", 3));
-        return new Basket();
+        return basket;
     }
 
 
@@ -22,7 +22,7 @@ class BasketTest {
         basket.add(new Product("name", 10));
 
         int expected = 1;
-        int actual = basket.getSize();
+        int actual = basket.size();
 
         assertEquals(expected, actual);
     }
@@ -35,7 +35,33 @@ class BasketTest {
         basket.add(null);
 
         int expected = 0;
-        int actual = basket.getSize();
+        int actual = basket.size();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldRemoveProduct() {
+        Basket basket = getFilledBasket();
+
+        basket.remove("product");
+
+        int expected = 2;
+        int actual = basket.size();
+
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    void shouldNotRemoveNotExistProduct() {
+        Basket basket = getFilledBasket();
+
+        basket.remove("dont exist");
+
+        int expected = 3;
+        int actual = basket.size();
 
         assertEquals(expected, actual);
     }

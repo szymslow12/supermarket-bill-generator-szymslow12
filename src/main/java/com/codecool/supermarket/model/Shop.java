0edbfd738dce7b9a138b75bb292c500fc986779a
szymslow12 <szym.slow12@gmail.com>
2018-09-20 12:13:09 +0200

    add loadProducts

--- src/main/java/com/codecool/supermarket/model/Shop.java
+++ src/main/java/com/codecool/supermarket/model/Shop.java
@@ -1,19 +1,35 @@
 package com.codecool.supermarket.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Shop {
 
-    private List<Product> products;
+    private List<Product> products = new LinkedList<>();
 
     public Shop(String pathToFile) {
-        products = loadProducts(pathToFile);
+        try {
+            loadProducts(pathToFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    private List<Product> loadProducts(String pathToFile) {
-        List<Product> products = new LinkedList<>();
-        return products;
+    private void loadProducts(String pathToFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(pathToFile));
+        String[] item;
+        while (scanner.hasNext()) {
+            item = scanner.nextLine().split(",");
+            String name = item[1];
+            int barcode = Integer.valueOf(item[0].trim());
+            int amount = Integer.valueOf(item[2].trim());
+            float price = Float.valueOf(item[3].trim());
+            products.add(new Product(name, barcode, price, amount));
+        }
     }
 
 

d9ce3e039870d1b8c68042b1f49622d12f828652
szymslow12 <szym.slow12@gmail.com>
2018-09-20 13:58:50 +0200

    implement main() in App

--- src/main/java/com/codecool/supermarket/App.java
+++ src/main/java/com/codecool/supermarket/App.java
@@ -1,9 +1,20 @@
 package com.codecool.supermarket;
 
+import com.codecool.supermarket.model.Basket;
+import com.codecool.supermarket.model.Shop;
+
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println(Arrays.toString(args));
+        if (args.length == 2) {
+            String pathToResource = "src/main/resources/";
+            Shop shop = new Shop(pathToResource + args[0]);
+            Basket basket = new Basket();
+            basket.loadBasketProducts(pathToResource + args[1]);
+            System.out.println("The total price is: " + shop.calculateBill(basket));
+        }
     }
 }

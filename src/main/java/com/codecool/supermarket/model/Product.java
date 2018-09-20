a9a0f3d88a05526d87218a209ea40940f822d3e8
szymslow12 <szym.slow12@gmail.com>
2018-09-20 10:04:32 +0200

    add Product.java with String name,int price,amount and setters/getters

---
+++ src/main/java/com/codecool/supermarket/model/Product.java
@@ -0,0 +1,56 @@
+package com.codecool.supermarket.model;
+
+public class Product {
+
+    private String name;
+    private int price;
+    private int amount = 1;
+
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    public Product(String name, int price, int amount) {
+        this(name, price);
+        this.amount = amount;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, Price: %s, Amount: %s", name, price, amount);
+    }
+}
